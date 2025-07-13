99efd637f1671842cd52802ced520281315e0478
Edwin Roldan <edwin@edwinroldan.dev>
2025-07-12 19:25:02 -0500

    setup frontend test with vitest
    # Conflicts:
    #	.gradle/8.14.3/checksums/checksums.lock
    #	.gradle/8.14.3/executionHistory/executionHistory.bin
    #	.gradle/8.14.3/executionHistory/executionHistory.lock
    #	.gradle/8.14.3/fileHashes/fileHashes.bin
    #	.gradle/8.14.3/fileHashes/fileHashes.lock
    #	.gradle/8.14.3/fileHashes/resourceHashesCache.bin
    #	.gradle/buildOutputCleanup/buildOutputCleanup.lock
    #	.gradle/buildOutputCleanup/cache.properties
    #	.gradle/buildOutputCleanup/outputFiles.bin
    #	build/resources/main/static/index.html

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,18 @@
-import { defineConfig } from 'vite'
+/*
+/ <reference types="vitest />
+/ <reference types="vite/client />
+*/
+
+import {defineConfig, type UserConfig} from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    css: true,
+    setupFiles: './tests/setup.ts'
+  }
+} as UserConfig)
